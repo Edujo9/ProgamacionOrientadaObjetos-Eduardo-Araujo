15bd25594d3d66e6e05319c2275df8375fa8a14e
EduardoAraujoAbad <44812964+EduardoAraujoAbad@users.noreply.github.com>
2018-11-13 12:42:30 -0500

    Add files via upload

---
+++ OperadoresLogicos.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class OperadoresLogicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.printf("%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n ",
+                "AND condicional (&&)", 
+                "false && false" ,(false & false),
+                "false && true",(false && true),
+                "true && false",(true && false),
+                "true && true" , (true && true));
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+                "OR condicional (||)",
+                "false || false",( false || false),
+                "false || true", (false || true),
+                "true || flase", (true || false),
+                "true || true", (true || true)); 
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+                "And Logico Boleano (&)",
+                "false & false",(false & false),
+                "false & true",(false & true),
+                "true & fals", (true & false),
+                "true & true", (true & true));
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+                "OR inclusivo logico booleano (|)",
+                "false & false",(false | false),
+                "false & true",(false | true),
+                "true & fals", (true | false),
+                "true & true", (true | true));
+        System.out.printf( "%s\n%s: %b\n%s: %b\n%s: %b\n%s: %b\n\n",
+                "OR exclusivo logico booleano (^)",
+                "false & false",(false ^ false),
+                "false & true",(false ^ true),
+                "true & fals", (true ^ false),
+                "true & true", (true ^ true)); 
+        System.out.printf("%s\n%s: %b\n%s: %b\n",
+                "No Logico (!)",
+                "!false",(!false),
+                "!true",(!true));
+    }
+    }
+    
+
